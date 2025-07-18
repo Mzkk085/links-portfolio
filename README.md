7e5229fc0f562538fcf4c5d725adbfbfb8d26035
Tulio Vítor <tuliovitor175@gmail.com>
2025-07-18 16:58:02 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# Portfólio de Tulio Vítor 🌐
+
+Este projeto está em desenvolvimento! Ainda vou adicionar:
+
+- Página "Sobre Mim"
+- Botões com links reais
+- Serviços oferecidos
+- Versão final do Portfólio
+
+📌 Tecnologias: HTML, CSS, JS
+📱 Design inicial pensado para mobile first
+
+🔗 Acesse a versão online (GitHub Pages): https://github.com/mzkk085
---
+++ scripts.js
@@ -0,0 +1,24 @@
+function toogleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // Quando mudar para light mode, mudar para outra imagem através da img no html
+
+  // Pegar a tag img
+  const img = document.querySelector(".profile img")
+
+  // Substituir a imagem
+  if (html.classList.contains("light")) {
+    // Se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "assets/avatar-light.png")
+  } else {
+    // Se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "assets/avatar.png")
+  }
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("alt", "Foto do Tulio, com fundo azul");
+  } else {
+    img.setAttribute("alt", "Foto do Tulio, com fundo laranja");
+  }
+}
