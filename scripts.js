327218186ffe180d269cb3a53cae4671912b24c5
Tulio Vítor <tuliovitor175@gmail.com>
2025-07-19 11:32:36 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,10 +10,10 @@ function toogleMode() {
   // Substituir a imagem
   if (html.classList.contains("light")) {
     // Se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "assets/avatar-light.svg")
+    img.setAttribute("src", "assets/avatar-light.png")
   } else {
     // Se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "assets/avatar.svg")
+    img.setAttribute("src", "assets/avatar.png")
   }
 
   if (html.classList.contains("light")) {
